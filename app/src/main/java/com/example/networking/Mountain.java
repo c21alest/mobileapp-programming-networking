fd864e7c1090f8f370b63c246d30b5529a3fc2bb
alexs <alexsanderstrom@outlook.com>
2022-04-29 16:11:49 +0200

    Lade till en class för berg som finns i array

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,29 @@
+package com.example.networking;
+
+/*
+[
+  {
+    "ID": "mobilprog_k2",
+    "name": "K2",
+    "type": "brom",
+    "company": "",
+    "location": "The Karakoram range",
+    "category": "",
+    "size": 8611,
+    "cost": 28251,
+    "auxdata": {
+      "wiki": "https://en.wikipedia.org/wiki/K2",
+      "img": "https://en.wikipedia.org/wiki/K2#/media/File:K2_2006b.jpg"
+    }
+ */
+
+public class Mountain {
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private String size;
+    private String cost;
+}
