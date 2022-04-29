98bb47989f0e6efd0ec30e79b9abcf3eaf3e32ad
alexs <alexsanderstrom@outlook.com>
2022-04-29 11:43:44 +0200

    Skapade en recycler view och dess komponenter som har koppling mot array

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
     ArrayList<String> Tracks;
+    RecyclerView myRecyclerView;
+    RecyclerView.Adapter myAdapter;
+    RecyclerView.LayoutManager myLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,14 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Tracks.add("Circuit de Spa-Francorchamps");
         Tracks.add("Silverstone Circuit");
         Tracks.add("Laguna Seca Raceway");
+
+        myRecyclerView = findViewById(R.id.recycler_view);
+        myLayoutManager = new LinearLayoutManager(this);
+        /*myAdapter = new MainAdapter(Tracks);
+
+         */
+        myRecyclerView.setAdapter(myAdapter);
+        myRecyclerView.setLayoutManager(myLayoutManager);
 
     }
 
