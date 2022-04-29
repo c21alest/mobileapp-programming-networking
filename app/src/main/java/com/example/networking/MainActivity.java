4209dda1b95c7578f4904ff0fcb415a5ffbde239
alexs <alexsanderstrom@outlook.com>
2022-04-29 11:37:30 +0200

    Skapade array med racing banor

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,11 +5,14 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+    ArrayList<String> Tracks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,12 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+
+        Tracks = new ArrayList<>();
+        Tracks.add("Circuit de Spa-Francorchamps");
+        Tracks.add("Silverstone Circuit");
+        Tracks.add("Laguna Seca Raceway");
+
     }
 
     @Override
